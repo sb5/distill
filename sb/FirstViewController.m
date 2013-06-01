d0aa538335fb13fead4d85ad8be0f52f89c8c555
sb main <steveb533@hotmail.com>
2013-06-01 09:45:49 -0400

    first

--- sb/FirstViewController.m
+++ sb/FirstViewController.m
@@ -19,7 +19,7 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     
-    //sb test
+    //sb test now from second comp
 }
 
 - (void)didReceiveMemoryWarning
