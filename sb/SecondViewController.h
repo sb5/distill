9b3e335a5527cc1f98b088ccbd672603a1e9c665
steve BREIGHNER <steveb533@hotmail.com>
2013-06-05 04:42:20 -0400

    Initial Commit

---
+++ sb/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  sb
+//
+//  Created by SB on 6/5/13.
+//  Copyright (c) 2013 SB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
---
+++ sb/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  sb
+//
+//  Created by SB on 6/5/13.
+//  Copyright (c) 2013 SB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
